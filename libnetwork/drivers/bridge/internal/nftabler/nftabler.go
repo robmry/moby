67e5a4e68b4f6822c0b50cb182b96062107b4bde
Rob Murray <rob.murray@docker.com>
2025-04-24 17:13:57 +0100

    Experimental! Reload nftables on SIGHUP
    Signed-off-by: Rob Murray <rob.murray@docker.com>

--- libnetwork/drivers/bridge/internal/nftabler/nftabler.go
+++ libnetwork/drivers/bridge/internal/nftabler/nftabler.go
@@ -5,10 +5,13 @@ package nftabler
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/docker/docker/libnetwork/drivers/bridge/internal/firewaller"
 	"github.com/docker/docker/libnetwork/internal/nftables"
 	"go.opentelemetry.io/otel"
+	"golang.org/x/sys/unix"
 )
 
 // Prefix for OTEL span names.
@@ -80,6 +83,20 @@ func NewNftabler(ctx context.Context, config firewaller.Config) (firewaller.Fire
 			return nil, fmt.Errorf("IPv6 initialisation: %w", err)
 		}
 	}
+
+	// FIXME(robmry) - locking!
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, unix.SIGHUP)
+	go func() {
+		for range c {
+			if nft.config.IPv4 {
+				nft.table4.Reload(ctx)
+			}
+			if nft.config.IPv6 {
+				nft.table6.Reload(ctx)
+			}
+		}
+	}()
 
 	return nft, nil
 }
