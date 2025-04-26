6a9cfe43fe5f8d008061df813103e017217425db
Rob Murray <rob.murray@docker.com>
2025-04-26 11:46:31 +0100

    nftabler: add network level rules
    Signed-off-by: Rob Murray <rob.murray@docker.com>

--- libnetwork/drivers/bridge/internal/nftabler/nftabler.go
+++ libnetwork/drivers/bridge/internal/nftabler/nftabler.go
@@ -36,6 +36,11 @@ const (
 	initialRuleGroup nftables.RuleGroup = iota
 )
 
+const (
+	fwdInICCRuleGroup = iota + initialRuleGroup + 1
+	fwdInFinalRuleGroup
+)
+
 type nftabler struct {
 	config firewaller.Config
 	table4 nftables.TableRef
