2bab2673a4794a08c6ba1f568566cdd80ee14cfd
Rob Murray <rob.murray@docker.com>
2025-05-15 18:18:21 +0100

    When switching between iptables/nftables, delete old rules
    TODO: add tests
    
    Signed-off-by: Rob Murray <rob.murray@docker.com>

---
+++ libnetwork/drivers/bridge/internal/nftabler/cleaner.go
@@ -0,0 +1,34 @@
+//go:build linux
+
+package nftabler
+
+import (
+	"context"
+	"os/exec"
+
+	"github.com/containerd/log"
+	"github.com/docker/docker/libnetwork/drivers/bridge/internal/firewaller"
+	"github.com/docker/docker/libnetwork/internal/nftables"
+)
+
+func Cleanup(ctx context.Context, config firewaller.Config) {
+	if config.IPv4 {
+		if err := exec.Command("nft", "delete", "table", string(nftables.IPv4), dockerTable).Run(); err != nil {
+			log.G(ctx).WithError(err).Info("Deleting nftables IPv4 rules")
+		} else {
+			log.G(ctx).Info("Deleted nftables IPv4 rules")
+		}
+	}
+	if config.IPv6 {
+		if err := exec.Command("nft", "delete", "table", string(nftables.IPv6), dockerTable).Run(); err != nil {
+			log.G(ctx).WithError(err).Info("Deleting nftables IPv6 rules")
+		} else {
+			log.G(ctx).Info("Deleted nftables IPv6 rules")
+		}
+	}
+	return
+}
+
+func (nft *nftabler) SetFirewallCleaner(fc firewaller.FirewallCleaner) {
+	nft.cleaner = fc
+}
