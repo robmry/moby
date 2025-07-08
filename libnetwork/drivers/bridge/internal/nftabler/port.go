972e32bb3010c3924f45092aca5ed42199ebf1ea
Rob Murray <rob.murray@docker.com>
2025-07-08 11:40:52 +0100

    Add nftables.AddChain
    And, make nftables.Chain return an invalid ref if the chain has
    not been added.
    
    Signed-off-by: Rob Murray <rob.murray@docker.com>

--- libnetwork/drivers/bridge/internal/nftabler/port.go
+++ libnetwork/drivers/bridge/internal/nftabler/port.go
@@ -11,6 +11,7 @@ import (
 	"strconv"
 
 	"github.com/containerd/log"
+	"github.com/docker/docker/errdefs"
 	"github.com/docker/docker/libnetwork/drivers/bridge/internal/firewaller"
 	"github.com/docker/docker/libnetwork/internal/nftables"
 	"github.com/docker/docker/libnetwork/types"
@@ -92,7 +93,11 @@ func (n *network) setPerPortForwarding(ctx context.Context, pbs []types.PortBind
 	if pbc.conf.Unprotected {
 		return nil
 	}
-	updateFwdIn := pbc.table.ChainUpdateFunc(ctx, chainFilterFwdIn(n.config.IfName), enable)
+	chainName := chainFilterFwdIn(n.config.IfName)
+	updateFwdIn := pbc.table.ChainUpdateFunc(ctx, chainName, enable)
+	if updateFwdIn == nil {
+		return errdefs.System(fmt.Errorf("setPerPortForwarding: no '%s' chain", chainName))
+	}
 	for _, pb := range pbs {
 		// When more than one host port is mapped to a single container port, this will
 		// generate the same rule for each host port. So, ignore duplicates when adding,
@@ -114,6 +119,9 @@ func (n *network) setPerPortForwarding(ctx context.Context, pbs []types.PortBind
 
 func (n *network) setPerPortDNAT(ctx context.Context, pbs []types.PortBinding, pbc pbContext, enable bool) error {
 	updater := pbc.table.ChainUpdateFunc(ctx, natChain, enable)
+	if updater == nil {
+		return errdefs.System(fmt.Errorf("setPerPortDNAT: no '%s' chain", natChain))
+	}
 	var proxySkip string
 	if !n.fw.config.Hairpin {
 		proxySkip = fmt.Sprintf("iifname != %s ", n.config.IfName)
@@ -153,7 +161,11 @@ func (n *network) setPerPortHairpinMasq(ctx context.Context, pbs []types.PortBin
 	if !n.fw.config.Hairpin {
 		return nil
 	}
-	updater := pbc.table.ChainUpdateFunc(ctx, chainNatPostRtIn(n.config.IfName), enable)
+	chainName := chainNatPostRtIn(n.config.IfName)
+	updater := pbc.table.ChainUpdateFunc(ctx, chainName, enable)
+	if updater == nil {
+		return errdefs.System(fmt.Errorf("setPerPortHairpinMasq: no '%s' chain", chainName))
+	}
 	for _, pb := range pbs {
 		// Nothing to do if NAT is disabled.
 		if pb.HostPort == 0 {
@@ -194,6 +206,9 @@ func (n *network) filterPortMappedOnLoopback(ctx context.Context, pbs []types.Po
 		return nil
 	}
 	updater := pbc.table.ChainUpdateFunc(ctx, rawPreroutingChain, enable)
+	if updater == nil {
+		return errdefs.System(fmt.Errorf("filterPortMappedOnLoopback: no '%s' chain", rawPreroutingChain))
+	}
 	for _, pb := range pbs {
 		// Nothing to do if not binding to the loopback address.
 		if pb.HostPort == 0 || !pb.HostIP.IsLoopback() {
